1146f23db333381ab4b74a42fb88092e53ef8684
Thomas Guignard <tom@timtom.ch>
2023-05-01 11:32:01 -0400

    Modified metagrid module to support IdRef lookup

--- js/unige-eth-metagrid/primo-explore-eth-metagrid/js/eth-metagrid.service.js
+++ js/unige-eth-metagrid/primo-explore-eth-metagrid/js/eth-metagrid.service.js
@@ -1,9 +1,9 @@
 export const ethMetagridService = ['$http', '$sce', function($http, $sce){
 
-    let baseurl = 'https://api.metagrid.ch/search?group=1&skip=0&take=50&provider=gnd&query=';
+    let baseurl = 'https://api.metagrid.ch/search?group=1&skip=0&take=50';
 
-    function getConcordances( gndId ){
-        let url = baseurl + gndId;
+    function getConcordances(metagridService,metagridId){
+        let url = baseurl + '&provider=' + metagridService + '&query=' + metagridId;
         $sce.trustAsResourceUrl(url);
 
         return $http.get(url)
