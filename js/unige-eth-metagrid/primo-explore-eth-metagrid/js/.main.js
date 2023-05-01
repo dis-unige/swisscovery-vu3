6396ec6430695fe761689bed2c46069a042cea07
Thomas Guignard <tom@timtom.ch>
2023-05-01 11:26:53 -0400

    Replace copy of ETH metagrid module with subtree

--- js/unige-eth-metagrid/primo-explore-eth-metagrid/js/.main.js
+++
@@ -1,8 +0,0 @@
-import 'primo-explore-eth-metagrid';
-
-var app = angular.module('viewCustom', ['ethMetagridModule']);
-
-app.component('prmServiceLinksAfter',  {
-        bindings: {parentCtrl: '<'},
-        template: `<eth-metagrid-component after-ctrl="$ctrl"></eth-metagrid-component>`
-    })
