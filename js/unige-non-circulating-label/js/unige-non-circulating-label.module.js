a2cae955c94368532456ec1ceb38ecd7dd7d1ddd
Thomas Guignard <tom@timtom.ch>
2023-05-01 15:08:01 -0400

    Fixed watch function to catch later loc changes

--- js/unige-non-circulating-label/js/unige-non-circulating-label.module.js
+++ js/unige-non-circulating-label/js/unige-non-circulating-label.module.js
@@ -4,41 +4,42 @@ angular
         var vm = this;
         this.$onInit = function() {
             
-            // Proceed only when holding locations details have been loaded
             $scope.$watch(
                 function () {
                     if (angular.isDefined(vm.parentCtrl.loc) && angular.isDefined(vm.parentCtrl.loc.items)) {
-                    return vm.parentCtrl.loc.items.length;
-                  }
-                  return 0;
+                        // As soon as there are location details, watch for changes in the list of location items
+                        return vm.parentCtrl.loc.items;
+                    }
+                    return 0;
                 },
-                function (newValue, oldValue) {
-                     if (newValue > 0) {
+                function () {
+                    // This listener function is called both during initial run and whenever the watched variable changes.
+                    if (angular.isDefined(vm.parentCtrl.loc)){
+                        // Prevent the listener from running when the locations list hasn't been defined yet (initial run)
+                             console.log(vm.parentCtrl.loc);
                          
-                         console.log(vm.parentCtrl.loc);
-                         
-                         // If a user is connected, the loanable status display is correct
-                         if (vm.parentCtrl.isLoggedIn()){
-                             // Do nothing for now. Could possibly edit 
-                             console.log('User is logged in, loanable status display correct.');
+                             // If a user is connected, the loanable status display is correct
+                             if (vm.parentCtrl.isLoggedIn()){
+                                 // Do nothing for now. Could possibly edit 
+                                 console.log('User is logged in, loanable status display correct.');
+                             }
+                             else {
+                                 // If no user is logged in, Primo displays a "not loanable" status even if this may not be the case.
+                                 // Replace that status with a note asking users to log in to get more info.
+                             
+                                 console.log('Found some holdings to correct');
+                             
+                                 var itemLocations = vm.parentCtrl.loc.items;
+                             
+                                  for(var i = 0; i < itemLocations.length; i++){
+                                      itemLocations[i].itemFields[1] = 'Veuillez vous connecter pour savoir si le document est empruntable et connaître la durée de prêt.';
+                                      //itemLocations[i]._additionalData.itemcategoryname = 'Veuillez vous connecter pour savoir si le document est empruntable et connaitre la durée de prêt';
+                                      // Not sure if this is reliable...
+                                      //console.log(itemLocations[i]);
+                                  }
+                             }
                          }
-                         else {
-                             // If no user is logged in, Primo displays a "not loanable" status even if this may not be the case.
-                             // Replace that status with a note asking users to log in to get more info.
-                             
-                             console.log('Found some holdings to correct');
-                             
-                             var itemLocations = vm.parentCtrl.loc.items;
-                             
-                              for(var i = 0; i < itemLocations.length; i++){
-                                  itemLocations[i].itemFields[1] = 'Veuillez vous connecter pour savoir si le document est empruntable et connaitre la durée de prêt';
-                                  itemLocations[i]._additionalData.itemcategoryname = 'Veuillez vous connecter pour savoir si le document est empruntable et connaitre la durée de prêt';
-                                  // Not sure if this is reliable...
-                                  //console.log(itemLocations[i]);
-                              }
-                         }
-                     }
-                }
+                    }
             );
         }
     }])
