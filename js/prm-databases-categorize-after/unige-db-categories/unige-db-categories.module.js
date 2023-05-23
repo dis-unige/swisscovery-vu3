d2df74592c2bbad9d06651f903ca162af2327055
Thomas Guignard <tom@timtom.ch>
2023-05-23 15:20:12 -0400

    Working rudimentary replacement menu

--- js/prm-databases-categorize-after/unige-db-categories/unige-db-categories.module.js
+++ js/prm-databases-categorize-after/unige-db-categories/unige-db-categories.module.js
@@ -1,3 +1,5 @@
+import {unigeDbCategoriesHtml} from './unige-db-categories.template.html';
+
 angular
     .module('unigeDbCategories', [])
     .controller('unigeDbCategoriesController', ['$scope', '$location', function ($scope, $location) {
@@ -9,21 +11,55 @@ angular
             
                 $scope.$watch(
                     function () {
-                        if (angular.isDefined(vm.parentCtrl.dbCategories) && angular.isDefined(vm.parentCtrl.dbCategories.dbcategory)){
-                            // As soon as there are location details, watch for changes in the list of location items
+                        if (angular.isDefined(vm.parentCtrl.dbCategories) && angular.isDefined(vm.parentCtrl.dbCategories.dbcategory[0].path)){
+                            // Wait until the dbCategories array has been fully created before going further
                             return vm.parentCtrl.dbCategories.dbcategory;
                         }
                         return 0;
                     },
                     function () {
                         // This listener function is called both during initial run and whenever the watched variable changes.
+                        
+                        vm.parentCtrl.dbOverrideCategories = [];
+                        
+                        let baseUrl = $location.$$absUrl.substring(0, $location.$$absUrl.indexOf('databases=category')+18);
+                        
                         if (angular.isDefined(vm.parentCtrl.dbCategories)){
                             console.log(vm.parentCtrl.dbCategories.dbcategory);
-                            let newDatabaseObject = new Object();
-                            newDatabaseObject.dbcategory = [{name:'Ouvrages de référence'}];
-                            vm.parentCtrl.dbCategories.dbcategory[9].dbcategories[0] = newDatabaseObject;
-                            console.log(vm.parentCtrl.dbCategories.dbcategory[9]);
-                            console.log(vm.parentCtrl.dbCategories.dbcategory[10]);
+                            
+                            vm.parentCtrl.dbCategories.dbcategory.forEach( (originalCategory) => {
+                                let newMenuEntry = new Object();
+                                newMenuEntry.link = baseUrl + ',' + originalCategory.path;
+                                if (angular.isDefined(originalCategory.displayValue)){
+                                    newMenuEntry.term = originalCategory.displayValue;
+                                }
+                                else {
+                                    newMenuEntry.term = originalCategory.name;
+                                }
+                                if (originalCategory.dbcategories[0].dbcategory.length > 0){
+                                    newMenuEntry.subcategories = [];
+                                    originalCategory.dbcategories[0].dbcategory.forEach( (subCategory) => {
+                                        let newSubMenuEntry = new Object();
+                                        if (angular.isDefined(subCategory.path)){
+                                            newSubMenuEntry.link = baseUrl + ',' + subCategory.path;
+                                        }
+                                        else {
+                                            newSubMenuEntry.link = baseUrl + ',' + originalCategory.name + '─' + subCategory.name;
+                                        }
+                                        if (angular.isDefined(subCategory.displayValue)){
+                                            newSubMenuEntry.term = subCategory.displayValue;
+                                        }
+                                        else {
+                                            newSubMenuEntry.term = subCategory.name;
+                                        }
+                                        newMenuEntry.subcategories.push(newSubMenuEntry);
+                                    })
+                                }
+                                vm.parentCtrl.dbOverrideCategories.push(newMenuEntry);
+                            });
+                            console.log(vm.parentCtrl.dbOverrideCategories);
+                            // Remove original categories menu
+                            document.getElementById('stickyDB').remove();
                         }
                     }
                 );
@@ -33,5 +69,5 @@ angular
     .component('prmDatabasesCategorizeAfter', {
         bindings: {parentCtrl: `<`},
         controller: 'unigeDbCategoriesController',
-        template: ''  
+        template: unigeDbCategoriesHtml 
     });
