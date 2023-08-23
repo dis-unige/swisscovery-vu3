0b012329d47a30ffbcc717ab14936b3377a8ec67
Thomas Guignard <tom@timtom.ch>
2023-08-23 17:12:45 +0200

    Replace empty status label with non-circulating

--- js/slsp-location-items-after/unige-non-circulating-label/README.md
+++ js/slsp-location-items-after/unige-non-circulating-label/README.md
@@ -8,11 +8,17 @@ policy display to be controlled by item or location policies.
 This is [a long-expected and heavily requested feature](https://ideas.exlibrisgroup.com/forums/308176-primo/suggestions/36317461-display-item-policy-in-item-record)
 that ExLibris has indicated is currently under development.
 
-Until this enhancement is released, many SLSP members have disabled the display of loan policies altogether to remove loanable items to be
-mistakenly labeled as unavailable, however this also means that items that are actually not available are not flagged as such.
+Until this enhancement is released, many SLSP members have disabled the display of 
+the "Not loanable" status, or even disable displaying loan policies altogether to remove loanable items to be
+mistakenly labeled as unavailable. However this also means that items that are actually not available are not flagged as such.
 
-This module attempts to provide a temporary solution to this issue. When browsing in guest mode (not logged in), the item policy display is modified to show a message
-inviting the user to log in in order to see whether an item is available or not. When a user is logged in, the item policy display is left as-is.
+This module attempts to provide a temporary solution to this issue. 
+When browsing in guest mode (not logged in), the item policy display is modified to show a message
+inviting the user to log in in order to see whether an item is available or not. 
+When a user is logged in, the module checks whether the item policy is left blank. If so, it means this is actually a real non loanable
+item except this message has been set as NOT_DEFINED to prevent it from mistakenly being displayed on Swisscovery.
+Consequently, when the item policy is blank, the module replaces it with a label identifying it as non-circulating.
+Other labels are left as-is.
 
 ![Screenshot of the Primo catalogue showing a different message next to an item depending whether the user is logged in or not.](unige-non-circulating-label-display.png)
 
@@ -56,15 +62,21 @@ var app = angular.module('viewCustom', [(...), 'unigeNonCirculatingLabel']);
 
 ### Dependencies
 
-This module requires the following label to be defined in order to properly display text in the correct language:
+This module requires the following labels to be defined in order to properly display text in the correct language:
 
 ```
-ViewIt Labels : nui.message.itemlogin
+ViewIt Labels : 
+
+nui.message.itemlogin
 
 EN : Please login to see if the<br/> document is available for loan
 FR : Veuillez vous connecter pour savoir<br/> si le document est empruntable
 DE : Bitte loggen Sie sich ein,<br/> um zu sehen ob das Dokument ausleihbar ist
 IT : Effettuare il login per sapere<br/> se il documento è disponibile per il prestito
+
+unige_non_circulating
+EN : Not loanable
+FR : Prêt exclu
 
 ```
 
@@ -74,6 +86,7 @@ No known issues at this time.
 
 ## Changelog
 
+* 2023-08-23 [TG] Changed logic to replace empty string with custom non-circulating label
 * 2023-06-14 [TG] Added documentation.
 * 2023-05-12 Pushed to production
 * 2023-04-27 [TG] Released module for internal testing.
--- js/slsp-location-items-after/unige-non-circulating-label/unige-non-circulating-label.module.js
+++ js/slsp-location-items-after/unige-non-circulating-label/unige-non-circulating-label.module.js
@@ -4,8 +4,10 @@ angular
         var vm = this;
         this.$onInit = function() {
             // Prepare message to be displayed
-            var displayMessage;
-            $translate('nui.message.itemlogin').then((translation) => {if (angular.isDefined(translation)){displayMessage = translation}});
+            var pleaseLogInMessage;
+            var nonCirculatingDisplay;
+            $translate('nui.message.itemlogin').then((translation) => {if (angular.isDefined(translation)){pleaseLogInMessage = translation}});
+            $translate('unige_non_circulating').then((translation) => {if (angular.isDefined(translation)){nonCirculatingDisplay = translation}});
             $scope.$watch(
                 function () {
                     if (angular.isDefined(vm.parentCtrl.loc) && angular.isDefined(vm.parentCtrl.loc.items)) {
@@ -18,23 +20,20 @@ angular
                     // This listener function is called both during initial run and whenever the watched variable changes.
                     if (angular.isDefined(vm.parentCtrl.loc)){
                         // Prevent the listener from running when the locations list hasn't been defined yet (initial run)
-
-                             // If a user is connected, the loanable status display is correct
-                             if (vm.parentCtrl.isLoggedIn()){
-                                 // Do nothing for now. Could possibly edit 
-                                 //console.log('User is logged in, loanable status display correct.');
-                             }
-                             else {                             
-                                 // If no user is logged in, Primo displays a "not loanable" status even if this may not be the case.
-                                 // Replace that status with a note asking users to log in to get more info.
-                             
-                                 //console.log('Found some holdings to correct');
-                             
-                                 var itemLocations = vm.parentCtrl.loc.items;
-                             
-                                  for(var i = 0; i < itemLocations.length; i++){
-                                      itemLocations[i].itemFields[1] = displayMessage;
-                                  }
+                             var itemLocations = vm.parentCtrl.loc.items;
+                             for(var i = 0; i < itemLocations.length; i++){
+                                 if (vm.parentCtrl.isLoggedIn()){
+                                     // If a user is connected, the loanable status display is correct
+                                     // except for when the display is empty.
+                                     if (itemLocations[i].itemFields[1] == " "){
+                                         itemLocations[i].itemFields[1] = nonCirculatingDisplay;
+                                     }
+                                 }
+                                 else {                             
+                                     // If no user is logged in, Primo displays a "not loanable" status even if this may not be the case.
+                                     // Replace that status with a note asking users to log in to get more info.
+                                     itemLocations[i].itemFields[1] = pleaseLogInMessage;
+                                 }
                              }
                          }
                     }
