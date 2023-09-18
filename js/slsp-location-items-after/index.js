318d87f5c1164793307fd510d7e92a4eaaf9c73b
Thomas Guignard <tom@timtom.ch>
2023-09-18 19:49:07 +0200

    Remove loan policy code from display

---
+++ js/slsp-location-items-after/index.js
@@ -0,0 +1,2 @@
+require('./unige-loan-policy-label-fix/unige-loan-policy-label-fix.module.js')
+module.exports = 'unigeLoanPolicyLabelFix'
---
+++ js/slsp-location-items-after/unige-loan-policy-label-fix/unige-loan-policy-label-fix.module.js
@@ -0,0 +1,32 @@
+angular
+    .module('unigeLoanPolicyLabelFix', [])
+    .controller('unigeLoanPolicyLabelFixController', ['$scope', function ($scope) {
+        var vm = this;
+        this.$onInit = function() {
+            $scope.$watch(
+                function () {
+                    if (angular.isDefined(vm.parentCtrl.loc) && angular.isDefined(vm.parentCtrl.loc.items)) {
+                        // As soon as there are location details, watch for changes in the list of location items
+                        return vm.parentCtrl.loc.items;
+                    }
+                    return 0;
+                },
+                function () {
+                    // This listener function is called both during initial run and whenever the watched variable changes.
+                    if (angular.isDefined(vm.parentCtrl.loc)){
+                        // Prevent the listener from running when the locations list hasn't been defined yet (initial run)
+                             var itemLocations = vm.parentCtrl.loc.items;
+                             for(var i = 0; i < itemLocations.length; i++){
+                                 // Remove the first 3 charachters from the loan policy display (internal codes)
+                                 itemLocations[i].itemFields[3] = itemLocations[i].itemFields[3].substring(3);
+                             }
+                         }
+                    }
+            );
+        }
+    }])
+    .component('slspLocationItemsAfter', {
+        bindings: {parentCtrl: `<`},
+        controller: 'unigeLoanPolicyLabelFixController',
+        template: ''  
+    });
